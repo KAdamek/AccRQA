b10a20c1e4cb7ef389ea459f8011ac3afef8877e
Jan Novotny <jan.novotny@physics.slu.cz>
2024-12-18 15:14:04 +0100

    Package for R in progress. accrqa_DET

--- R/RAccRQA.R
+++ R/RAccRQA.R
@@ -1,14 +1,81 @@
-dyn.load("libAccRQA_R.so")
+#dyn.load("libAccRQA_R.so")
+#' @useDynLib AccRQA, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+NULL
 
-accrqa_DET <- function(input, tau_values, emb_values, lmin_values, threshold_values, norm, calc_ENTR)
+#' Calculate Determinism for Cross-Recurrence Quantification Analysis
+#'
+#' This function calculates the determinism (DET) for cross-recurrence quantification analysis (CRQA)
+#' based on a set of input parameters, including time delay, embedding dimensions, minimum line length,
+#' threshold values, and normalization.
+#'
+#' @param input A numeric matrix or data frame representing the input data for CRQA analysis.
+#' @param tau_values A numeric vector specifying the time delay(s) to be used in the analysis.
+#' @param emb_values A numeric vector specifying the embedding dimensions to be tested.
+#' @param lmin_values A numeric vector specifying the minimum diagonal line lengths for DET computation.
+#' @param threshold_values A numeric vector specifying the threshold values for recurrence computation.
+#' @param norm A character string specifying the normalization method to be used. Options may include
+#'   `"euclidean"`, `"maximal"`, etc.
+#' @param calc_ENTR A logical value indicating whether to calculate entropy (ENTR) along with DET.
+#' @param comp_platform A character string specifying the computing platform. Options may include
+#'   `"cpu"`, `"nv_gpu"`, etc.
+#' @return A list containing:
+#'   - `DET`: The determinism values computed for the given input parameters.
+#'   - `ENTR` (if `calc_ENTR = TRUE`): The entropy values corresponding to the DET computations.
+#'
+#' @details
+#' The function performs cross-recurrence quantification analysis (CRQA) using the specified parameters.
+#' DET measures the proportion of recurrent points forming diagonal lines in the recurrence plot,
+#' which indicates deterministic structures in the data. If `calc_ENTR` is `TRUE`, the entropy of diagonal
+#' line lengths is also computed.
+#'
+#' @examples
+#' # Example usage
+#' input_data <- matrix(runif(100), nrow = 10)
+#' tau <- c(1, 2)
+#' emb <- c(2, 3)
+#' lmin <- 1
+#' threshold <- 1
+#' norm_method <- "euclidean"
+#' calculate_entropy <- TRUE
+#' comp_platform <- "cpu"
+#'
+#' results <- accrqa_DET(
+#'   input = input_data,
+#'   tau_values = tau,
+#'   emb_values = emb,
+#'   lmin_values = lmin,
+#'   threshold_values = threshold,
+#'   norm = norm_method,
+#'   calc_ENTR = calculate_entropy,
+#'   platform = comp_platform,
+#' )
+#'
+#' @export
+accrqa_DET <- function(input, tau_values, emb_values, lmin_values, threshold_values, norm, calc_ENTR, platform)
 {
   input_size <- length(input)
   nTaus <- length(tau_values)
   nEmbs <- length(emb_values)
   nLmins <- length(lmin_values)
-  nThresholds <- length(thresholds)
+  nThresholds <- length(threshold_values)
   output_size <- nTaus*nEmbs*nLmins*nThresholds*5
   if(input_size < 1) stop("n must be a positive integer!")
+  
+  norm_method <- switch(
+    norm,
+    "euclidean" = 1,
+    "maximal" = 2,
+    0  # Default case
+  )
+  
+  comp_platform <- switch(
+    platform,
+    "cpu" = 1,
+    "nv_gpu" = 1024,
+    0
+  )
+
   rst <- .C("R_double_accrqa_DET",
     output = double(length=output_size),
     as.double(input),
@@ -21,10 +88,12 @@ accrqa_DET <- function(input, tau_values, emb_values, lmin_values, threshold_val
     as.integer(nLmins),
     as.double(threshold_values),
     as.integer(nThresholds),
-    as.integer(norm),
-    as.integer(calc_ENTR)
+    as.integer(norm_method),
+    as.integer(calc_ENTR),
+    as.integer(comp_platform)
   )
-  result <- data.frame(det = rst[["detp"]], l = rst[["lp"]], lmax = rst[["lmaxp"]], entr = rst[["entr"]])
+  
+  result <- data.frame(rst) #data.frame(det = rst[["detp"]], l = rst[["lp"]], lmax = rst[["lmaxp"]], entr = rst[["entr"]])
   return(result)
 }
 
--- src/AccRQA_R_bindings.cpp
+++ src/AccRQA_R_bindings.cpp
@@ -2,6 +2,40 @@
 
 #include <R.h>
 #include "../include/AccRQA_library.hpp"
+#include <R_ext/Rdynload.h>
+
+
+Accrqa_CompPlatform check_comp_platform2(int int_comp_platform, Accrqa_Error *error){
+	Accrqa_CompPlatform comp_platform;
+	if(int_comp_platform==1){
+		comp_platform = PLT_CPU;
+	}
+	else if(int_comp_platform==1024){
+		comp_platform = PLT_NV_GPU;
+	}
+	else {
+		printf("Error: Invalid computing platform.\n");
+		comp_platform = PLT_ERROR;
+		*error = ERR_INVALID_ARGUMENT;
+	}
+	return(comp_platform);
+}
+
+Accrqa_Distance check_distance_type2(int int_distance_type, Accrqa_Error *error){
+	Accrqa_Distance distance_type;
+	if(int_distance_type==1){
+		distance_type = DST_EUCLIDEAN;
+	}
+	else if(int_distance_type==2){
+		distance_type = DST_MAXIMAL;
+	}
+	else {
+		distance_type = DST_ERROR;
+		printf("Error: Invalid distance type.\n");
+		*error = ERR_INVALID_ARGUMENT;
+	}
+	return(distance_type);
+}
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,98 +52,31 @@ extern "C" {
 		int    *nLmins,
 		double *threshold_values, 
 		int    *nThresholds, 
-		int    *distance_type,
-		int    *calc_ENTR
+		int    *int_distance_type,
+		int    *calc_ENTR, 
+		int    *int_comp_platform
 	){
 		size_t local_input_size = (size_t) input_size[0];
 		int local_nThresholds = nThresholds[0];
 		int local_nTaus = nTaus[0];
 		int local_nEmbs = nEmbs[0];
 		int local_nLmins = nLmins[0];
-		int local_distance_type = distance_type[0];
+		int local_int_distance_type = int_distance_type[0];
 		int local_calc_ENTR = calc_ENTR[0];
+		int local_int_comp_platform = int_comp_platform[0];
 		Accrqa_Error error = SUCCESS;
-		
-		accrqa_DET(
-			output, 
-			input, 
-			local_input_size, 
-			tau_values,
-			local_nTaus,
-			emb_values,
-			local_nEmbs,
-			lmin_values,
-			local_nLmins,
-			threshold_values, 
-			local_nThresholds, 
-			local_distance_type,
-			local_calc_ENTR,
-			&error
-		);
-	}
 	
-	void R_double_accrqa_LAM(
-		double *output, 
-		double *input, 
-		int    *input_size, 
-		int    *tau_values,
-		int    *nTaus,
-		int    *emb_values,
-		int    *nEmbs,
-		int    *vmin_values,
-		int    *nVmins,
-		double *threshold_values, 
-		int    *nThresholds, 
-		int    *distance_type,
-		int    *calc_ENTR
-	){
-		size_t local_input_size = (size_t) input_size[0];
-		int local_nThresholds = nThresholds[0];
-		int local_nTaus = nTaus[0];
-		int local_nEmbs = nEmbs[0];
-		int local_nVmins = nVmins[0];
-		int local_distance_type = distance_type[0];
-		int local_calc_ENTR = calc_ENTR[0];
-		Accrqa_Error error = SUCCESS;
-		
-		accrqa_LAM(
-			output, 
-			input, 
-			local_input_size, 
-			tau_values,
-			local_nTaus,
-			emb_values,
-			local_nEmbs,
-			vmin_values,
-			local_nVmins,
-			threshold_values, 
-			local_nThresholds, 
-			local_distance_type,
-			local_calc_ENTR,
-			&error
-		);
-	}
+		Accrqa_Distance distance_type = check_distance_type2(local_int_distance_type, &error);
+		//std::cout << "Distance type: "<< distance_type << " " << error <<std::endl;
+		if(error != SUCCESS){
+		  return;
+		} 
 
-	void R_double_accrqa_RR(
-		double *output, 
-		double *input, 
-		int    *input_size, 
-		int    *tau_values,
-		int    *nTaus,
-		int    *emb_values,
-		int    *nEmbs,
-		double *threshold_values, 
-		int    *nThresholds, 
-		int    *distance_type
-	){
-		size_t local_input_size = (size_t) input_size[0];
-		int local_nThresholds = nThresholds[0];
-		int local_nTaus = nTaus[0];
-		int local_nEmbs = nEmbs[0];
-		int local_distance_type = distance_type[0];
-		Accrqa_Error error = SUCCESS;
-		
-		accrqa_RR(
+		Accrqa_CompPlatform comp_platform = check_comp_platform2(local_int_comp_platform, &error);
+		//std::cout << "Computation platform: " << comp_platform << " " << error <<std::endl;
+		if(error != SUCCESS) return;
+
+		accrqa_DET(
 			output,
 			input,
 			local_input_size,
@@ -117,13 +84,109 @@ extern "C" {
 			local_nTaus,
 			emb_values,
 			local_nEmbs,
-			threshold_values, 
-			local_nThresholds, 
-			local_distance_type,
+			lmin_values,
+			local_nLmins,
+			threshold_values,
+			local_nThresholds,
+			distance_type,
+			local_calc_ENTR,
+			comp_platform,
 			&error
 		);
 	}
 	
+//	void R_double_accrqa_LAM(
+//		double *output, 
+//		double *input, 
+//		int    *input_size, 
+//		int    *tau_values,
+//		int    *nTaus,
+//		int    *emb_values,
+//		int    *nEmbs,
+//		int    *vmin_values,
+//		int    *nVmins,
+//		double *threshold_values, 
+//		int    *nThresholds, 
+//		int    *distance_type,
+//		int    *calc_ENTR
+//	){
+//		size_t local_input_size = (size_t) input_size[0];
+//		int local_nThresholds = nThresholds[0];
+//		int local_nTaus = nTaus[0];
+//		int local_nEmbs = nEmbs[0];
+//		int local_nVmins = nVmins[0];
+//		int local_distance_type = distance_type[0];
+//		int local_calc_ENTR = calc_ENTR[0];
+//		Accrqa_Error error = SUCCESS;
+//		
+//		accrqa_LAM(
+//			output, 
+//			input, 
+//			local_input_size, 
+//			tau_values,
+//			local_nTaus,
+//			emb_values,
+//			local_nEmbs,
+//			vmin_values,
+//			local_nVmins,
+//			threshold_values, 
+//			local_nThresholds, 
+//			local_distance_type,
+//			local_calc_ENTR,
+//			&error
+//		);
+//	}
+
+//	void R_double_accrqa_RR(
+//		double *output, 
+//		double *input, 
+//		int    *input_size, 
+//		int    *tau_values,
+//		int    *nTaus,
+//		int    *emb_values,
+//		int    *nEmbs,
+//		double *threshold_values, 
+//		int    *nThresholds,
+//		int    *comp_platform,
+//		int    *distance_type
+//	){
+//		size_t local_input_size = (size_t) input_size[0];
+//		int local_nThresholds = nThresholds[0];
+//		int local_nTaus = nTaus[0];
+//		int local_nEmbs = nEmbs[0];
+//		int local_distance_type = distance_type[0];
+//		int local_int_comp_platform = comp_platform[0];
+//		Accrqa_Error error = SUCCESS;
+//
+//		Accrqa_CompPlatform local_comp_platform = check_comp_platform(local_int_comp_platform, error);
+//		
+//		accrqa_RR(
+//			output,
+//			input,
+//			local_input_size,
+//			tau_values,
+//			local_nTaus,
+//			emb_values,
+//			local_nEmbs,
+//			threshold_values, 
+//			local_nThresholds, 
+//			local_distance_type,
+//			local_comp_platform,
+//			&error
+//		);
+//	}
+
+  R_CMethodDef cMethods[] = {
+    {"R_double_accrqa_DET", (DL_FUNC) &R_double_accrqa_DET, 14},
+//    {"R_double_accrqa_LAM", (DL_FUNC) &R_double_accrqa_LAM, 12},
+//    {"R_double_accrqa_RR", (DL_FUNC) &R_double_accrqa_RR, 10},
+    {NULL, NULL, 0}
+  };
+  
+  void R_init_AccRQA(DllInfo *info){
+    R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, FALSE);
+  }	
 	
 #ifdef __cplusplus
 }
